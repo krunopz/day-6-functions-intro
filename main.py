d65d29ceaedf24ea00ce36686ad08e4121f35f9b
Krunoslav Stefanek <prof.stefanek@gmail.com>
2021-01-14 20:12:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#FUNCTIONS in Python
+
+#this is the way we define functions
+def my_function():
+  print("This is some kind of function!")
+  print("Bye!")
+
+def my_addition(a, b):
+  sum=a+b
+  print(sum)
+
+#this is how we call the function:
+my_function()
+#upper function does not need any inputs so the parentheses are blank
+my_addition(1,2)
